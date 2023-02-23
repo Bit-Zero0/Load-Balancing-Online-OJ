da0727aafa1b35a134c32e170e5399c357a6abbe
Fengalex0 <3153366821@qq.com>
2023-02-23 22:51:56 +0800

    编译服务器(日志系统已完成)

---
+++ compile_server/compiler.hpp
@@ -0,0 +1,69 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+
+#include "../comm/util.hpp"
+#include "../comm/log.hpp"
+
+namespace ns_compiler
+{
+    using namespace ns_util;
+    using namespace ns_log;
+
+    class Compiler
+    {
+    public:
+        Compiler(){};
+
+        ~Compiler(){};
+
+        static bool Compile(const std::string &file_name)
+        {
+            pid_t pid = fork();
+            if (pid < 0)
+            {
+                LOG(ERROR) << "内部错误，创建子进程失败"
+                           << "\n";
+                return false;
+            }
+            else if (pid == 0)
+            {
+                int _stderr = open(PathUtil::Stderr(file_name).c_str(), O_CREAT | O_WRONLY, 0644);
+                if (_stderr < 0)
+                {
+                    LOG(WARNING) << "没有成功形成stderr文件"
+                                 << "\n";
+                    exit(1);
+                }
+
+                dup2(_stderr, 2); // 重定向标准错误到_stderr
+
+                // 程序替换，并不影响进程的文件描述符表
+                execlp("g++", "g++", "-o", PathUtil::Exe(file_name).c_str(),
+                       PathUtil::Src(file_name).c_str(), "-D", "COMPILER_ONLINE", "-std=c++11", nullptr);
+
+                LOG(ERROR) << "启动编译器g++失败，可能是参数错误"
+                           << "\n";
+                exit(2);
+            }
+            else
+            {
+                waitpid(pid, nullptr, 0);
+                if (FileUtil::IsFileExists(file_name))
+                {
+                    LOG(INFO) << PathUtil::Src(file_name) << " 编译成功!"
+                              << "\n";
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    };
+}
