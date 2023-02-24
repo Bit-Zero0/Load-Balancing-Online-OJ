bff244681c27530ead80d35b9bd2b0dbfcb2e0a3
Fengalex0 <3153366821@qq.com>
2023-02-24 22:05:28 +0800

    读写文件；获取唯一文件名等方法

--- comm/util.hpp
+++ comm/util.hpp
@@ -6,11 +6,33 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <sys/time.h>
+#include <fstream>
+#include <atomic>
 
 const std::string temp_path = "./temp/";
 
 namespace ns_util
 {
+	class TimeUtil
+    {
+    public:
+        static std::string GetTimeStamp()
+        {
+            struct timeval _time;
+            gettimeofday(&_time, nullptr);
+
+            return std::to_string(_time.tv_sec);
+        }
+
+        static std::string GetTimeMs()
+        {
+            struct timeval _time;
+            gettimeofday(&_time, nullptr);
+
+            return std::to_string(_time.tv_sec * 1000 + _time.tv_usec / 1000);
+        }
+    };
+	
     class PathUtil
     {
     public:
@@ -23,6 +45,7 @@ namespace ns_util
         }
 
     public:
+		//// 编译时需要有的临时文件
         // 构建源文件路径+后缀的完整文件名
         static std::string Src(const std::string &file_name)
         {
@@ -35,10 +58,27 @@ namespace ns_util
             return AddSuffix(file_name, "exe");
         }
 
+		static std::string CompileError(const std::string &file_name)
+        {
+            return AddSuffix(file_name, ".compile_error");
+        }
+
+
+		//// 运行时需要的临时文件
         // 构建该程序对应的标准错误完整的路径+后缀名
         static std::string Stderr(const std::string &file_name)
         {
             return AddSuffix(file_name, "stderr");
+        }
+		
+		static std::string Stdin(const std::string &file_name)
+        {
+            return AddSuffix(file_name, ".stdin");
+        }
+
+        static std::string Stdout(const std::string &file_name)
+        {
+            return AddSuffix(file_name, ".stdout");
         }
     };
 
@@ -56,17 +96,49 @@ namespace ns_util
 
             return false;
         }
-    };
-
-    class TimeUtil
-    {
-    public:
-        static std::string GetTimeStamp()
+		
+		static std::string UniqueFileName()
         {
-            struct timeval _time;
-            gettimeofday(&_time, nullptr);
+            static std::atomic_uint id(0);
+            ++id;
+            std::string ms = TimeUtil::GetTimeMs();
+            std::string unique_id = std::to_string(id);
+            return ms + "_" + unique_id;
+        }
 
-            return std::to_string(_time.tv_sec);
+        static bool WriteFile(const std::string &target, const std::string &content)
+        {
+            std::ofstream out(target);
+            if (!out.is_open())
+            {
+                return false;
+            }
+
+            out.write(content.c_str(), content.size());
+            out.close();
+            return true;
+        }
+
+        static bool ReadFile(const std::string target, std::string *content, bool KeepSymbol = false)
+        {
+            (*content).clear();
+
+            std::ifstream in(target);
+            if (in.is_open())
+            {
+                return false;
+            }
+
+            std::string line;
+
+            while (std::getline(in, line))
+            {
+                (*content) += line;
+                (*content) += (KeepSymbol ? "\n" : "");
+            }
+
+            in.close();
+            return true;
         }
     };
 }
